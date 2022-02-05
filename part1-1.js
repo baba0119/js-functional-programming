af391aaad1ffac63229e7a27fbf27faa299f4caa
baba0119 <70310558+baba0119@users.noreply.github.com>
2022-02-05 16:45:51 +0900

    p23まで

--- part1-1.js
+++ part1-1.js
@@ -18,3 +18,33 @@ printMessage('msg', 'h1', 'Hello World');
 var printMessage = run(addToDom('msg'), h1, echo);
 
 printMessage('Hello World');
+
+//
+// p8 リスト1.2
+//
+var printMessage = run(console.log, repeat(2), h2, echo);
+
+printMessage('Get Functional');
+
+//
+// p9
+// アロー関数で書き変えた
+//
+console.log(
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(num => Math.pow(num, 2))
+);
+
+//
+// p23 リスト1.5
+//
+let enrollment = [
+  {enrolled: 2, grade: 100},
+  {enrolled: 2, grade: 80},
+  {enrolled: 1, grade: 89}
+];
+
+_.chain(enrollment)
+  .filter(student => student.enrolled > 1)
+  .pluck('grade')
+  .average()
+  .value();
